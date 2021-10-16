4db2ff3a4716600ee84262770a76bf44bee183d2
gregorylira <gregory.lira007@gmail.com>
2021-10-16 08:20:31 -0300

    entidade + migration create preco sugerido

--- src/database/migrations/1634308665082-CreatePrecosSugeridos.ts
+++ src/database/migrations/1634308665082-CreatePrecosSugeridos.ts
@@ -1,11 +1,73 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {Column, MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class CreatePrecosSugeridos1634308665082 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "precosSugeridos",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "produto",
+                        type: "varchar"
+                    },
+                    {
+                        name: "custo_MP",
+                        type: "float"
+                    },
+                    {
+                        name: "custo_total",
+                        type: "float"
+                    },
+                    {
+                        name: "preco_min",
+                        type: "float"
+                    },
+                    {
+                        name: "preco_max",
+                        type: "float"
+                    },
+                    {
+                        name: "preco_medio",
+                        type: "float"
+                    },
+                    {
+                        name: "seu_preco_venda",
+                        type: "float"
+                    },
+                    {
+                        name: "imposto_sob_venda",
+                        type: "float"
+                    },
+                    {
+                        name: "margem_contribuicao",
+                        type: "float"
+                    },
+                    {
+                        name: "margem_liquida",
+                        type: "float"
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()",
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "now()"
+                    }
+                ]
+        }))
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("precosSugeridos")
     }
 
 }
---
+++ src/entity/Preco_Sugerido.ts
@@ -0,0 +1,55 @@
+import {Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn} from "typeorm";
+import { v4 as uuid } from "uuid"
+
+@Entity("precosSugeridos")
+class Insumo {
+    
+    @PrimaryColumn()
+    readonly id: string;
+
+    @Column()
+    produto: string;
+
+    @Column("float")
+    custo_MP: number;
+
+    @Column("float")
+    custo_total: string;
+
+    @Column("float")
+    preco_min: number;
+
+    @Column("float")
+    preco_max: number;
+
+    @Column("float")
+    preco_medio: number;
+
+    @Column("float")
+    seu_preco_venda: number;
+
+    @Column("float")
+    imposto_sob_venda: number;
+
+    @Column("float")
+    margem_contribuicao: number;
+
+    @Column("float")
+    margem_liquida: number;
+    
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+
+    constructor(){
+        if (!this.id){
+            this.id = uuid();
+        }
+    }
+
+
+}
+
+export { Insumo }
