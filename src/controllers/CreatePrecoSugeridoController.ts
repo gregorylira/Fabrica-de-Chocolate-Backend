6ffba9ea2491863389463812af413a3f263bdea7
gregorylira <gregory.lira007@gmail.com>
2021-11-12 21:59:35 -0300

    #03 - mudança -> criação do controller de produtos para aceitar 1 json + rota

--- src/controllers/CreatePrecoSugeridoController.ts
+++ src/controllers/CreatePrecoSugeridoController.ts
@@ -9,16 +9,25 @@ class CreatePrecoSugeridoController {
         const arr = request.body
         var listaPrecosSugeridos = []
 
-        for( var val of arr){
-            const {produto,custo_MP,custo_total,preco_min,preco_max,preco_medio,seu_preco_venda,imposto_sob_venda,margem_contribuicao,margem_liquida} = val
+        if (Array.isArray(arr)) {
+            for( var val of arr){
+                const {produto,custo_MP,custo_total,preco_min,preco_max,preco_medio,seu_preco_venda,imposto_sob_venda,margem_contribuicao,margem_liquida} = val
+                const createPrecoSugeridoService = new CreatePrecoSugeridoService()
+
+                const precoProduto = await createPrecoSugeridoService.execute({produto,custo_MP,custo_total,preco_min,preco_max,preco_medio,seu_preco_venda,imposto_sob_venda,margem_contribuicao,margem_liquida})
+                listaPrecosSugeridos.push(precoProduto)
+            }
+            return response.json(listaPrecosSugeridos)
+        }else{
+            const {produto,custo_MP,custo_total,preco_min,preco_max,preco_medio,seu_preco_venda,imposto_sob_venda,margem_contribuicao,margem_liquida} = arr
             const createPrecoSugeridoService = new CreatePrecoSugeridoService()
 
             const precoProduto = await createPrecoSugeridoService.execute({produto,custo_MP,custo_total,preco_min,preco_max,preco_medio,seu_preco_venda,imposto_sob_venda,margem_contribuicao,margem_liquida})
-            listaPrecosSugeridos.push(precoProduto)
+            
+            return response.json(precoProduto)
+
         }
 
-
-        return response.json(listaPrecosSugeridos)
     }
 }
 
--- src/routes.ts
+++ src/routes.ts
@@ -49,7 +49,7 @@ router.delete("/composicao-adicional", deleteComposicaoAdicionalController.handl
 
 
 router.put("/insumo/:id", modifyInsumoService.handle)
-router.put("/preco-sugerido", modifyPrecoSugeridoController.handle)
+router.put("/preco-sugerido/:id", modifyPrecoSugeridoController.handle)
 router.put("/composicao", modifyComposicaoProdutoController.handle)
 router.put("/composicao-adicional", modifyComposicaoAdicionalProdutoController.handle)
 
