5c2dcb47c14af4efc4017b3c403c3ddcac34b953
gregorylira <gregory.lira007@gmail.com>
2021-11-13 10:27:38 -0300

    mudança no controller de composicao

--- src/controllers/CreateComposicaoController.ts
+++ src/controllers/CreateComposicaoController.ts
@@ -1,4 +1,7 @@
 import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { InsumoRepositories } from "../repositories/InsumosRepositories";
+import { PrecoSugeridoRepositories } from "../repositories/PrecoSugeridoRepositories";
 import { CreateComposicaoService } from "../service/CreateComposicaoService";
 
 
@@ -9,15 +12,44 @@ class CreateComposicaoController {
         const arr = request.body
         var listaComposicao = []
         
-        for(var val of arr){
-            const {produtoFK, ingredienteFK, quant_liqu}= val
+        if(Array.isArray(arr)){
+            for(var val of arr){
+                const {produtoFK, ingredienteFK, quant_liqu}= val
+                const createComposicaoService = new CreateComposicaoService
+
+                const comp = await createComposicaoService.execute({produtoFK, ingredienteFK, quant_liqu})
+                listaComposicao.push(comp)
+            }
+            
+            return response.json(listaComposicao)
+        }
+        else{
+            var {produtoFK, ingredienteFK, quant_liqu}= arr
+
+            const precoSugeridoRepositories = getCustomRepository(PrecoSugeridoRepositories)
+            const produto = await precoSugeridoRepositories.findOne({ where: { produto:produtoFK } })
+
+
+            const ingredienteRespositorie = getCustomRepository(InsumoRepositories)
+            const ingrediente = await ingredienteRespositorie.findOne({ where: { produto:ingredienteFK } })
+
+
             const createComposicaoService = new CreateComposicaoService
 
+            produtoFK = produto.id
+            ingredienteFK = ingrediente.id
+
+
             const comp = await createComposicaoService.execute({produtoFK, ingredienteFK, quant_liqu})
-            listaComposicao.push(comp)
+            const composicao = {
+                produtoFK: produto.produto,
+                ingredienteFK: ingrediente.produto,
+                quant_liqu: comp.quant_liqu,
+                unid_medidaFK: comp.unid_medidaFK,
+                custo_insumoFK: comp.custo_InsumoFK,
+            }
+            return response.json(composicao)
         }
-        
-        return response.json(listaComposicao)
     }
 }
 
